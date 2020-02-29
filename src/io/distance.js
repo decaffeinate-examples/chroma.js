64439d6f8bbb53c6bc671951721c4b5f20ded6ed
decaffeinate <decaffeinatebot@gmail.com>
2020-02-29 02:31:02 +0000

    decaffeinate: Run post-processing cleanups on delta-e.coffee and 1 other file

--- src/io/distance.js
+++ src/io/distance.js
@@ -1,3 +1,14 @@
+/* eslint-disable
+    camelcase,
+    func-names,
+    guard-for-in,
+    no-cond-assign,
+    no-param-reassign,
+    no-restricted-syntax,
+    no-undef,
+*/
+// TODO: This file was created by bulk-decaffeinate.
+// Fix any style issues and re-enable lint.
 /*
  * decaffeinate suggestions:
  * DS102: Remove unnecessary code created because of implicit returns
@@ -7,19 +18,20 @@
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
 // simple Euclidean distance
-chroma.distance = function(a, b, mode) {
-    // Delta E (CIE 1976)
-    // see http://www.brucelindbloom.com/index.html?Equations.html
-    let needle, needle1;
-    if (mode == null) { mode = 'lab'; }
-    if ((needle = type(a), ['string', 'number'].includes(needle))) { a = new Color(a); }
-    if ((needle1 = type(b), ['string', 'number'].includes(needle1))) { b = new Color(b); }
-    const l1 = a.get(mode);
-    const l2 = b.get(mode);
-    let sum_sq = 0;
-    for (let i in l1) {
-        const d = (l1[i] || 0) - (l2[i] || 0);
-        sum_sq += d*d;
-    }
-    return Math.sqrt(sum_sq);
+chroma.distance = function (a, b, mode) {
+  // Delta E (CIE 1976)
+  // see http://www.brucelindbloom.com/index.html?Equations.html
+  let needle; let
+    needle1;
+  if (mode == null) { mode = 'lab'; }
+  if ((needle = type(a), ['string', 'number'].includes(needle))) { a = new Color(a); }
+  if ((needle1 = type(b), ['string', 'number'].includes(needle1))) { b = new Color(b); }
+  const l1 = a.get(mode);
+  const l2 = b.get(mode);
+  let sum_sq = 0;
+  for (const i in l1) {
+    const d = (l1[i] || 0) - (l2[i] || 0);
+    sum_sq += d * d;
+  }
+  return Math.sqrt(sum_sq);
 };
